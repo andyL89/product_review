fae2a03b552e3595563474ea20b1e98499cdfda3
Andy Lawson <alawson89@gmail.com>
2021-03-19 09:34:34 -0700

    add reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_160718) do
+ActiveRecord::Schema.define(version: 2021_03_19_163235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2021_03_19_160718) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.text "content_body"
+    t.integer "rating"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reviews", "products"
 end
